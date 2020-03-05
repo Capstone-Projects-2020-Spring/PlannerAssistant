578dcfb8419d88d516829d3e4f9718a71434f089
stevennardo <steven.nardo@temple.edu>
2020-03-04 19:12:26 -0500

    UI Framework with Routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//file and page imports
+import './index.css';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Notfound from './components/Notfound';
+
+import User from "./imgs/user.png";
+
+import * as serviceWorker from './serviceWorker';
+//import * as firebase from 'firebase';
+
+//firebase.initalizeApp(config)
+//TODO: get config information from firebase console
+
+const routing = (
+  <Router>
+  <div>
+    <div id='nav'>
+      <Link to="/"><div class='links' id="title"> Planner Assistant</div></Link>
+      <Link to="/profile"><div class='links' id="user"> <img alt="userimg" src={User}/></div></Link>
+    </div>
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

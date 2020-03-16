5bddc4348a0302f5ed9b6b13e44e3f97f0270dc1
Rahshon <tuf70447@temple.edu>
2020-03-16 05:27:18 -0700

    More code containing a very basic approach to parsing lines of text

---
+++ src/parse.js
@@ -0,0 +1,17 @@
+let dinnerPlan = "Dinner at Olive Garden";
+let lunchPlan = "Lunch near South Street";
+
+
+function Event(Dinner){
+    this.dinner = dinner;
+
+};
+
+//  /at/
+
+const regE = regExp('at');
+let match;
+
+while((match = regexp.exec(dinnerPlan)) != null){
+  
+}

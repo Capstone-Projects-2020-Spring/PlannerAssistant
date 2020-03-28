222ecaad96fe8b7af50966a5e96c5b00afaaf2fd
stevennardo <steven.nardo@temple.edu>
2020-03-28 19:52:58 -0400

    origin of server code

---
+++ src/main/java/com/example/demo/API_Call.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class API_Call {
+	
+	@GetMapping("query")
+    public String Query(@RequestParam(value = "queryString", required = true) String queryString) {
+
+		String user_query = queryString;
+		
+		//Call any functions here to do any analysis, 
+		//store those Java files in this project and create the class instances in this files constructor
+		
+        //This string should be the JSON Object response thats converted into a string via GSON thats to be output to the user
+		return ""; 
+    }
+	
+}

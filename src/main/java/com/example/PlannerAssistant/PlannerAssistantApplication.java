13526f75c0c27690f7f356869c750766bd8d0c00
Rahshon <tuf70447@temple.edu>
2020-04-11 20:07:46 -0700

    fixed server code. Should be clear of config errors

---
+++ src/main/java/com/example/PlannerAssistant/PlannerAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlannerAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlannerAssistant/PlannerAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlannerAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
